b4529c828538d5f1bd9fd1f5a3c5f7e3f010a298
nicrausaz <n.crausaz99@gmail.com>
2017-07-13 08:21:47 +0200

    all mails sendings are working

--- API/class/MailSender.php
+++ API/class/MailSender.php
@@ -2,27 +2,24 @@
   class MailSender {
     public function toApplicant ($to) {
       require('templates/mailApplicant.php');
-      mail($to, $subject, $message, $headers); // working
+      mail($to, $subject, $message, $headers);
     }
 
-    public function toStarsRatings ($recipients, $job, $path) {
+    public function toStarsRatings ($to, $job, $path, $key, $value) {
       require('templates/mailStars.php');
-      foreach ($recipients as $email) {
-        mail($to, $subject, $message, $headers);
-      }
+      mail('nicolas.crausaz@epfl.ch', $subject, $message, $headers); //$to
     }
 
     public function toProjectResp ($project, $path) {
       require('templates/mailProjectResp.php');
-      mail($to, $subject, $message, $headers); // working
+      mail($to, $subject, $message, $headers);
     }
 
     public function getRecipients ($ratings, $job, $path) {
-      $recipients = [];
       foreach ($ratings as $key => $value) {
-        array_push($recipients, $this->getMailingList($key, $value));
+        $email = strtolower($this->getMailingList($key, $value));
+        $this->toStarsRatings($email, $job, $path, $key, $value);
       }
-      $this->toStarsRatings($recipients, $job, $path);
     }
 
     public function getMailingList ($key, $value) {
--- API/templates/mailStars.php
+++ API/templates/mailStars.php
@@ -1,10 +1,10 @@
 <?php
     $subject = 'New LIPID appliance';
     $message = "Hi," ."\n\n". 
-    "Someone just applied to a $job  position at LIPID." . "\n\n".
-    "He/she mentioned $$$$$$energy as their area of interest ($$numStars)";
+    "Someone just applied to a $job position at LIPID." . "\n\n".
+    "He/she mentioned $key as their area of interest ($value stars)" . "\n\n";
 
-    $message .= "$PATH";
+    $message .= "Find data here: " . $path;
     $headers = 'From: noreply+lipid-form@epfl.ch' . "\r\n" .
     'Reply-To: noreply+lipid-form@epfl.ch' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
